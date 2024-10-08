2b11ddf6e56d4149c42b754bd0e7c902081a5aca
donaldinou <donaldinou@gmail.com>
2024-10-08 14:29:57 +0200

    feat(test) : unit test for Tools

---
+++ Tests/phpunit/Util/Tools.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Part of SplTypes package.
+ *
+ * (c) Adrien Loyant <donald_duck@team-df.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Ducks\Component\SplTypes\Tests\phpunit\Util;
+
+use Ducks\Component\SplTypes\Tests\common\Month;
+use Ducks\Component\SplTypes\Util\Tools;
+use PHPUnit\Framework\TestCase;
+
+class ToolsTest extends TestCase
+{
+    /**
+     * Unit test.
+     *
+     * @return void
+     */
+    public function test(): void
+    {
+        $this->assertTrue(Tools::isSplEnumExists(Month::class));
+    }
+
+    /**
+     * Test bad instantiation of Tools Helper.
+     *
+     * @return void
+     *
+     * @covers Tools::__construct()
+     */
+    public function testNew(): void
+    {
+        $reflection = new \ReflectionClass(Tools::class);
+        $constructor = $reflection->getConstructor();
+
+        $this->assertInstanceOf(\ReflectionMethod::class, $constructor);
+        $this->assertFalse($constructor->isPublic());
+    }
+}
